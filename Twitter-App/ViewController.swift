9165e500e4a5d3f51eb3b7e6afa4015513d6615c
Rachel Thomas <racheltho@gmail.com>
2015-02-17 10:15:07 -0800

    Initial Commit

---
+++ Twitter-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter-App
+//
+//  Created by Rachel Thomas on 2/17/15.
+//  Copyright (c) 2015 Rachel Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Twitter-AppTests/Twitter_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Twitter_AppTests.swift
+//  Twitter-AppTests
+//
+//  Created by Rachel Thomas on 2/17/15.
+//  Copyright (c) 2015 Rachel Thomas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Twitter_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
