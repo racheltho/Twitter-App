4440364dffc45c572b0b3ce81cce1f30fa2a760a
Rachel Thomas <racheltho@gmail.com>
2015-02-20 20:46:36 -0800

    adding gitignore

---
+++ GlobalConstants.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalConstants.swift
+//  Twitter-App
+//
+//  Created by Rachel Thomas on 2/20/15.
+//  Copyright (c) 2015 Rachel Thomas. All rights reserved.
+//
+
+import UIKit
+
+class GlobalConstants: NSObject {
+   
+}
